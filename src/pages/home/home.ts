b5c8384897acc91bba05d8c3ef65956e685bc691
ancelinSerre <ancelin.serre@etu.univ-grenoble-alpes.fr>
2018-03-14 15:46:14 +0100

    page home basics

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,47 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  results: Result[];
+  searchQuery: string = "";
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  // Appelé à chaque fois que tu écris dans la search bar
+  getResults(ev: any) {
+    // val récupère la valeur contenue dans l'input de la search bar
+    let val = ev.target.value;
+    return val ? this.results = tabResults : this.results = [];
+  }
 }
+
+// Interface pour décrire un résultat de recherche
+export interface Result {
+  title: string;
+  author: string;
+  date: number; // faudra metter un object;
+  image: string;
+}
+
+
+const tabResults: Result[] = [
+  {
+    title: "La cambrioleuse",
+    author: "Jacquie",
+    date: 1,
+    image: "https://pbs.twimg.com/profile_images/627117609444581380/7YG7kxA4_400x400.png"
+  },
+  {
+    title: "Fast and Furious 51",
+    author: "Marc Bide",
+    date: 2,
+    image: "https://pbs.twimg.com/profile_images/888969645/30116_117264888296255_117264014963009_164865_6947075_n_400x400.jpg"
+  },
+  {
+    title: "Plus belle la vie - Le Film",
+    author: "Jeanine Cagole",
+    date: 3,
+    image: "http://scrat.hellocoton.fr/img/classic/gipsy-rapper-reprend-wiggle-de-jason-derulo-16711334.jpg"
+  }
+];
