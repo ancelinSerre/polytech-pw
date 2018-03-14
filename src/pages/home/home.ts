d76479b7aa415e88dfde39930ec4693312e8eeb3
ancelinSerre <ancelin.serre@etu.univ-grenoble-alpes.fr>
2018-03-14 16:11:03 +0100

    affichage de no results fonctionnel

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgIf } from "@angular/common";
 
 @Component({
   selector: 'page-home',
@@ -8,7 +9,6 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   results: Result[];
-  searchQuery: string = "";
 
   constructor(public navCtrl: NavController) {
 
