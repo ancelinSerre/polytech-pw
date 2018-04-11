e47bdd49c96b9f55054bee1e1652240925bc3839
Ancelin Serre <ancelin.serre@etu.univ-grenoble-alpes.fr>
2018-04-11 14:54:25 +0200

    Fin tp3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Shake } from '@ionic-native/shake';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -19,7 +20,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    HttpClientModule
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -30,6 +31,7 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     StatusBar,
     SplashScreen,
+    Shake,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,24 +3,35 @@ import { DetailsPage } from '../details/details';
 import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { apiKey } from '../../app/tmdb';
+import { AlertController, NavController, Platform } from 'ionic-angular';
+import { Subscription } from 'rxjs/Subscription';
+import { Shake } from '@ionic-native/shake';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
   results: Observable<Result[]>;
   pushPage: any;
-  /**
-   * Constructeur de la page
-   * @param navCtrl 
-   */
-  constructor(public http: HttpClient) {
+  private shakeSubscription: Subscription;
+
+  constructor(public http: HttpClient, private alertCtrl: AlertController,public navCtrl: NavController, 
+    public platform: Platform, private shake: Shake) {
     this.results = Observable.of([]);
     this.pushPage = DetailsPage;
   }
 
+  ionViewDidEnter() {
+    this.shakeSubscription = Observable.fromPromise(this.platform.ready())
+    .switchMap( () => this.shake.startWatch())
+    .switchMap( () => this.discoverMovies())
+    .subscribe( movies => this.showRandomMovieAlert(movies));
+  }
+  ionViewWillLeave() {
+    this.shakeSubscription.unsubscribe();
+  }
   /**
    * Listener pour l'évènement écriture dans la search bar
    * @param ev variable d'environnement
@@ -54,7 +65,45 @@ export class HomePage {
       /* Pluck nous permet de récupérer uniquement le champ 'results' */
     }).pluck('results');
   }
+
+  private discoverMovies() : Observable<Result[]>
+  {
+    let url : string = 'https://api.themoviedb.org/3/discover/movie'
+    return this.http.get<Result[]>(url,
+    {
+      params:
+      {
+        api_key: apiKey,
+        primary_release_year: '2018',
+        language: 'fr',
+      }
+    }).pluck('results');
+  }
+
+  private showRandomMovieAlert(movies: Result[]) : void
+  {
+    /* On sélectionne un film au hasard parmi le tableau donné */
+    let movie : Result = movies[Math.floor(Math.random()*movies.length)];
+    /* On créé une alerte avec les informations du film */
+    let alert = this.alertCtrl.create({
+      title: movie.title,
+      message: movie.overview,
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Details',
+          handler: () => {
+            this.navCtrl.push(this.pushPage,movie);
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
+
 
 /* Interface pour décrire un résultat de recherche */
 export interface Result {  
